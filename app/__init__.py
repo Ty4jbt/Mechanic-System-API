e79164c885d6c69506e32b30582e63f502a07a22
Ty4jbt <tyaboe@gmail.com>
2025-04-13 03:44:59 -0700

    Added Swagger and added customer routes

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,19 @@ from .blueprints.mechanics import mechanics_bp
 from .blueprints.service_ticket import service_ticket_bp
 from .blueprints.inventory import inventory_bp
 from .blueprints.orders import orders_bp
+from flask_swagger_ui import get_swaggerui_blueprint
+
+# Swagger UI configuration
+SWAGGER_URL = '/api/docs'
+API_URL = '/static/swagger.yaml'
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Mechanics API",
+    }
+)
 
 def create_app(config_name):
 
@@ -24,5 +37,6 @@ def create_app(config_name):
     app.register_blueprint(service_ticket_bp, url_prefix='/service_tickets')
     app.register_blueprint(inventory_bp, url_prefix='/inventory')
     app.register_blueprint(orders_bp, url_prefix='/orders')
+    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
     return app
